1210eba2b2c031774f415e644e0b085edc309f87
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-25 21:53:34 +0300

    написаны три программы

---
+++ Exam_001/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a, b, c, d, S; // задали тип данных
+Console.Write("a: ");
+a = Convert.ToInt32(Console.ReadLine ()); //для конвератции из стринг в int
+Console.Write("b: ");
+b = Convert.ToInt32(Console.ReadLine ());
+Console.Write("c: ");
+c = Convert.ToInt32(Console.ReadLine ());
+Console.Write("d: ");
+d = Convert.ToInt32(Console.ReadLine ());
+S = (a+b+c+d)/4;
+Console.WriteLine (S);
+
