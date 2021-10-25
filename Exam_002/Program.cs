d1cb4ece60c3de6e48f0183c73be8a36d281de2a
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-25 21:57:49 +0300

    написаны три программы

---
+++ Exam_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// максимальное из двух
+int x, y, max;
+Console.Write ("Введите первое число: ");
+x=Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+y=Convert.ToInt32 (Console.ReadLine());
+if (x>y)
+{
+    max=x;
+}
+else
+{
+    max=y;
+    }
+Console.Write ("Максимальное число: ");
+Console.WriteLine (max);
+Console.WriteLine ("максимальное значение: "+max); // второй вариант написания
